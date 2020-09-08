27b66000bcfffcc5cc1a462a22592fce13db77f2
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2020-09-07 21:20:22 -0600

    Add files via upload

--- Temperature_Storage_Test.py
+++ Temperature_Storage_Test.py
@@ -17,9 +17,12 @@ y = []
 def TempConversion(c):
     return c * 9.0 / 5.0 + 32
 
-def write_temp(temp):
+def write_die_temp(temp):
     with open("/home/pi/die_temp.csv", "a") as log:
         log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
+def write_obj_temp(temp2):
+    with open("/home/pi/obj_temp.csv", "a") as log:
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp2)))
 
 def graph(temp):
     y.append(temp)
@@ -41,14 +44,18 @@ sensor.begin()
 # Loop printing measurements every second.
 print ('Press Ctrl-C to quit.')
 while True:
-    write_temp(die_temp)
-    # graph(die_temp)
 
     obj_temp = sensor.readObjTempC()
+    temp2 = obj_temp
+    
     die_temp = sensor.readDieTempC()
+    temp = obj_temp
     #reads object temp
     print ('Object temperature: {0:0.3F}*C / {1:0.3F}*F'.format(obj_temp, TempConversion(obj_temp)))
     #reads die temp -
     print ('Die temperature: {0:0.3F}*C / {1:0.3F}*F'.format(die_temp, TempConversion(die_temp)))
     #time.sleep(1.0)
+    write_die_temp(temp)
+    write_obj_temp(temp2)
+    # graph(die_temp)
     plt.pause(1)
