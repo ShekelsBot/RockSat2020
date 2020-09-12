79dcceb664280d4a05a3ef90b3fc29fdc5b65d0b
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2020-09-11 22:26:55 -0600

    Add files via upload
    Added support for 2nd temperature sensor
    Added support for data storage for both
    Added Graphing capabilities

--- Temperature_Storage_Test.py
+++ Temperature_Storage_Test.py
@@ -6,6 +6,7 @@
 # TMP006 Adafruit Module
 
 from time import sleep, strftime, time
+#import busio
 import matplotlib.pyplot as plt
 import Adafruit_TMP.TMP006 as TMP006
 
@@ -17,12 +18,19 @@ y = []
 def TempConversion(c):
     return c * 9.0 / 5.0 + 32
 
-def write_die_temp(temp):
+def write_die_temp(die_1): #die temp file SENSOR 1
     with open("/home/pi/die_temp.csv", "a") as log:
-        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
-def write_obj_temp(temp2):
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(die_1)))
+def write_obj_temp(obj_1): #obj temp file SENSOR 2
     with open("/home/pi/obj_temp.csv", "a") as log:
-        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp2)))
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(obj_1)))
+
+def write_die_temp_2(die_2): #obj temp file SENSOR 2
+    with open("/home/pi/obj_temp.csv", "a") as log:
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(die_2)))
+def write_obj_temp_2(obj_2): #obj temp file SENSOR 2
+    with open("/home/pi/obj_temp.csv", "a") as log:
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(obj_2)))
 
 def graph(temp):
     y.append(temp)
@@ -34,28 +42,42 @@ def graph(temp):
 
 # Initialze sensor
 sensor = TMP006.TMP006()
+sensor_2 = TMP006.TMP006()
 
 # Default i2C address is 0x40 and bus is 1.
+#i2c = busio.I2C(board.SCL, board.SDA)
 sensor = TMP006.TMP006(address=0x40, busnum=1)
+sensor_2 = TMP006.TMP006(address=0x41, busnum =1) #change 3v to ad0
 
 # Default rate is 16 samples per loop.
 sensor.begin()
-
+sensor_2.begin()
 # Loop printing measurements every second.
 print ('Press Ctrl-C to quit.')
 while True:
 
-    obj_temp = sensor.readObjTempC()
-    temp2 = obj_temp
-    
     die_temp = sensor.readDieTempC()
-    temp = obj_temp
+    die_1 = die_temp
+    obj_temp = sensor.readObjTempC()
+    obj_1 = obj_temp
+
+    die_temp2 = sensor_2.readDieTempC()
+    die_2 = die_temp2
+    obj_temp2 = sensor_2.readObjTempC()
+    obj_2= obj_temp2
+
     #reads object temp
+    #reads die temp
     print ('Object temperature: {0:0.3F}*C / {1:0.3F}*F'.format(obj_temp, TempConversion(obj_temp)))
-    #reads die temp -
     print ('Die temperature: {0:0.3F}*C / {1:0.3F}*F'.format(die_temp, TempConversion(die_temp)))
-    #time.sleep(1.0)
-    write_die_temp(temp)
-    write_obj_temp(temp2)
-    # graph(die_temp)
+    write_die_temp(die_1)
+    write_obj_temp(obj_1)
+
+    time.sleep(0.1) #prevents crash from interference
+
+    print ('Object temperature: {0:0.3F}*C / {1:0.3F}*F'.format(obj_temp2, TempConversion(obj_temp2)))
+    print ('Die temperature: {0:0.3F}*C / {1:0.3F}*F'.format(die_temp2, TempConversion(die_temp2)))
+    write_die_temp_2(die_2)
+    write_obj_temp_2(obj_2)
+    # graph(die_temp) - Graphing function for testing
     plt.pause(1)
