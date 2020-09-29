ab460ecfbe3ca9448f26a4777908372dd2bc1084
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2020-09-29 09:08:23 -0600

    Add files via upload
    Added serial connection support.

--- Temperature_Test.py
+++ Temperature_Test.py
@@ -6,7 +6,18 @@
 # TMP006 Adafruit Module
 
 import time
+import serial
 import Adafruit_TMP.TMP006 as TMP006
+
+# Define serial port
+ser = serial.Serial(
+        port='/dev/ttyAMA0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
 
 # Function to convert celsius (c) to fahrenhiet.
 def TempConversion(c):
@@ -28,6 +39,11 @@ while True:
     die_temp = sensor.readDieTempC()
     #reads object temp
     print ('Object temperature: {0:0.3F}*C / {1:0.3F}*F'.format(obj_temp, TempConversion(obj_temp)))
-    #reads die temp -
+    #reads die temp
     print ('Die temperature: {0:0.3F}*C / {1:0.3F}*F'.format(die_temp, TempConversion(die_temp)))
     time.sleep(1.0)
+
+    #Sends data over seial connection
+    ser.write("Serial Connection: OBJ temperature is: {0:0.3F}*C / {1:0.3F}*F".format(obj_temp, TempConversion(obj_temp)))
+    ser.write("Serial Connection: DIE temperature is: {0:0.3F}*C / {1:0.3F}*F".format(die_temp, TempConversion(die_temp)))
+    time.sleep(1)
