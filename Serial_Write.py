af36fbf604321904bc643b7ba87f2bec81c41306
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2020-10-02 15:25:06 -0600

    Update Serial_Write.py

--- Serial_Write.py
+++ Serial_Write.py
@@ -8,7 +8,7 @@ import time
 import serial
 
 ser = serial.Serial(
-        port='/dev/ttyAMA0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+        port='/dev/ttyS0)', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
         baudrate = 9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
