a44914e7f20952ee90cad20611d48c56eb419385
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2020-12-04 12:57:46 -0700

    Add files via upload
    Added File Transfer function. Need to change it to check for either the Logical Names or have it scan for the connected drives. The Mount Name changes randomly.

--- USB_Power.sh
+++ USB_Power.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+DATE=$(date +%C%y%m%d)
+#Get current date format in Century Year Month Day
+#Corresponds to specific folder name
+cd uhubctl
+#Navitage to specific directory
 umount /media/usb-drive
+#Unmount Camera - if it is mounted
 uhubctl -a off -l 1-1
+#Turn off power to the USB 2.0 Ports
 sleep 5
 echo "Turning on power to USB Ports"
 uhubctl -a on -l 1-1
+#Turn power back on the USB Ports
 sleep 5
 echo "Mounting Camera"
-mount /dev/sda1 /mnt/usb-drive
-echo "All done"
+mount /dev/sdc1 /mnt/usb-drive #Change to logical name -Mount names change on reboot
+#Remount Camera
+echo "Camera is mounted under /media/usb-drive/"
+cd /mnt/usb-drive/DCIM/$DATE
+#Navigate to Specifc directory
+#Use DATE value for specifc name of folder
+echo "Copying AB.MP4 files"
+find -iname '*AB.MP4' -exec cp {} /home/pi/data/ \;
+#2 copies of files. AA is high quality. AB is lower quality.
+echo "Finished"
