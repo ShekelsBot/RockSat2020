e63380e4af8b4349780a563a8dcab15e90255bfb
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2020-12-06 13:14:38 -0700

    Add files via upload

--- USB_Power.sh
+++ USB_Power.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+# Purpose of this is to mount and turn off power to USB ports so the camera can record
+# Then transfer data once it has finished recording
+# 
+# Script uses Uhubctl
+# Needs the drive to mount based on UUID
+#
+# https://github.com/mvp/uhubctl
+# https://www.cyberciti.biz/faq/linux-mount-an-lvm-volume-partition-command/
+#
 DATE=$(date +%C%y%m%d)
 #Get current date format in Century Year Month Day
 #Corresponds to specific folder name
 cd uhubctl
-#Navitage to specific directory
-umount /media/usb-drive
-#Unmount Camera - if it is mounted
 uhubctl -a off -l 1-1
 #Turn off power to the USB 2.0 Ports
 sleep 5
 echo "Turning on power to USB Ports"
 uhubctl -a on -l 1-1
 #Turn power back on the USB Ports
-sleep 5
 echo "Mounting Camera"
-mount /dev/sdc1 /mnt/usb-drive #Change to logical name -Mount names change on reboot
-#Remount Camera
+mount -a
 echo "Camera is mounted under /media/usb-drive/"
 cd /mnt/usb-drive/DCIM/$DATE
 #Navigate to Specifc directory
