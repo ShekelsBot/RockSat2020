324f899203bb0f082fec5cf698b270f900b7c97e
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2020-12-11 13:12:06 -0700

    Add files via upload
    Multiprocessing added

--- Telemetry_Final.py
+++ Telemetry_Final.py
@@ -1,7 +1,7 @@
 # Andrew Bruckbauer
-# 8/18/2020
-# Main Telelmetry model
-# Combines all previous iterations of code from the repository to create one module.
+# 12/11/2020
+# Telemetry Module 3.0
+# Combines all modules
 
 # Documentation on sensors
 # https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi
@@ -28,30 +28,22 @@
 # Adafruit Motor Hat
 # Pi Camera
 
+from multiprocessing import Process
+import board, busio, os, serial, picamera
+import psutil
 import time
 from time import strftime
-import serial
-import board
-import busio
 import Adafruit_TMP.TMP006 as TMP006
 import adafruit_vl53l0x
 import adafruit_adxl34x
-import picamera
-import os
-import psutil
 
 MAX_FILES = 999
 DURATION = 20
 SPACE_LIMIT = 80
 TIME_STATUS_OK = 0.5
-
 file_root = "/home/pi/videos/"
 
-# Initialze sensors
-# Initilize i2c bus and distance sensor
 i2c = busio.I2C(board.SCL, board.SDA)
-vl53 = adafruit_vl53l0x.VL53L0X(i2c)
-accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
 # Define serial port
 ser = serial.Serial(
@@ -63,29 +55,102 @@ ser = serial.Serial(
         timeout=1
 )
 
-# Function to convert celsius (c) to fahrenhiet.
 def TempConversion(c):
     return c * 9.0 / 5.0 + 32
 
-# Function to write data to a .csv file for graphing
-def write_sensors(die_1): 
+def write_sensors(): 
     with open("/home/pi/data/Telemetry.csv", "a") as log:
         log.write("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17}\n"
-        .format(strftime("%Y-%m-%d %H:%M:%S"),"Sensor 1",str(die_1)+" F",str(obj_1)+" F",str(die_temp)+" C",str(obj_temp)+" C",
-        " ","Sensor 2",str(die_2)+" F",str(obj_2)+" F",str(die_temp2)+" C",str(obj_temp2)+" C"," ","Sensor 3",str(distance)+" MM"
+        .format(strftime("%Y-%m-%d %H:%M:%S"),"Temp1",str(TempConversion(die1))+" F",str(TempConversion(obj1))+" F",str(die1)+" C",str(obj1)+" C",
+        " ","Temp2",str(TempConversion(die2))+" F",str(TempConversion(obj2))+" F",str(die2)+" C",str(obj2)+" C",
+        " ","Distance","BLANK MM"
         ,str(xAxis),str(yAxis),str(zAxis)))
 
-# Define both Temp Sensors
-sensor = TMP006.TMP006()
-sensor_2 = TMP006.TMP006()
-# Address both Temp Sensors
-sensor = TMP006.TMP006(address=0x40, busnum=1) # Default i2C address is 0x40 and bus is 1.
-sensor_2 = TMP006.TMP006(address=0x41, busnum =1) # change 3v to ad0
+def sensors():
+    # Temperature Sensor 1
+    sensor1 = TMP006.TMP006()
+    sensor1 = TMP006.TMP006(address=0x40, busnum=1) # Default i2C address is 0x40 and bus is 1.
+    sensor1.begin()
+    # Temperature Sensor 2
+    sensor2 = TMP006.TMP006()
+    sensor2 = TMP006.TMP006(address=0x41, busnum=1) #change 3v to ad0
+    sensor2.begin()
+    # Accelerometer Sensor
+    accelerometer = adafruit_adxl34x.ADXL345(i2c)
+    # Distance Sensor
+    vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
-# Default rate is 16 samples per loop.
-# Initilize both Temp Sensors
-sensor.begin()
-sensor_2.begin() 
+    while True:
+        # Distance Sensor
+        global distance
+        distance = vl53.range
+        print ("Range: {0}mm".format(distance))
+        ser.write (b'Range: %d '%(distance)+b' mm \n')
+        time.sleep(.1)
+
+        # Temperature Sensor 1
+        global obj1
+        global die1
+        obj1 = sensor1.readObjTempC()
+        die1 = sensor1.readDieTempC()
+
+        # Temperature Sensor 2
+        global obj2
+        global die2
+        obj2 = sensor2.readObjTempC()
+        die2 = sensor2.readDieTempC()
+
+        # Temperature Sensor 1 Serial out
+        ser.write (b'Sensor 1 object Temperature: %d \n'%(obj1))
+        ser.write (b'Sensor 1 die Temperature: %d \n'%(die1))
+        time.sleep(.1)
+
+        # Temperature Sensor 2 Serial out
+        ser.write (b'Sensor 2 object Temperature: %d \n'%(obj2))
+        ser.write (b'Sensor 2 die Temperature: %d \n'%(die2))
+        time.sleep(.1)
+
+        # Accelerometer tupple parse
+        global xAxis
+        global yAxis
+        global zAxis
+        xAxis = (round(accelerometer.acceleration[0],1))
+        yAxis = (round(accelerometer.acceleration[1],1))
+        zAxis = (round(accelerometer.acceleration[2],1))
+
+        # Accelerometer Serial out
+        ser.write (b'X Axis: %d \n'%(xAxis))
+        ser.write (b'Y Axis: %d \n'%(yAxis))
+        ser.write (b'Z Axis: %d \n'%(zAxis))
+        
+        #Write all data
+        write_sensors()
+        time.sleep(1)
+
+# Pi Camera function
+def secondCamera():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1920,1080)
+        camera.framerate = 30
+
+        print('Searching files...')
+        for i in range(1, MAX_FILES):
+            file_number = i
+            file_name = file_root + "video" + str(i).zfill(3) + ".h264"
+            exists = os.path.isfile(file_name)
+            if not exists:
+                print ("Search Complete")
+                break
+
+        for file_name in camera.record_sequence(file_root + "video%03d.h264" % i for i in range(file_number, MAX_FILES)):
+            timeout = time.time() + DURATION
+            print('Recording to %s' % file_name)
+            
+            while(time.time() < timeout):
+                time.sleep(TIME_STATUS_OK)
+                if(psutil.disk_usage(".").percent > SPACE_LIMIT):
+                    print('WARNING: Low space!')
+                    break
 
 # Loop printing measurements every second.
 # Tell where stuff is being stored
@@ -94,79 +159,14 @@ print ('Storing data /home/pi/data')
 print ('Temperature is in Fahrenheit')
 print ('Distance is in MM')
 
-"""
+# Space check limit 
 if(psutil.disk_usage(".").percent > SPACE_LIMIT):
     print('WARNING: Low space!')
     exit()
-
-with picamera.PiCamera() as camera:
-    camera.resolution = (1920,1080)
-    camera.framerate = 30
-
-    print('Searching files...')
-    for i in range(1, MAX_FILES):
-        file_number = i
-        file_name = file_root + "video" + str(i).zfill(3) + ".h264"
-        exists = os.path.isfile(file_name)
-        if not exists:
-            print ("Search Complete")
-            break
-
-    for file_name in camera.record_sequence(file_root + "video%03d.h264" % i for i in range(file_number, MAX_FILES)):
-        timeout = time.time() + DURATION
-        print('Recording to %s' % file_name)
-
-        while(time.time() < timeout):
-            time.sleep(TIME_STATUS_OK)
-            if(psutil.disk_usage(".").percent > SPACE_LIMIT):
-                print('WARNING: Low space!')
-                break;
-"""
-# Main loop
-while True:
-
-    # Variable Juggling for Temp Sensor 1
-    die_temp = sensor.readDieTempC()
-    die_1 = TempConversion(die_temp)
-    obj_temp = sensor.readObjTempC()
-    obj_1 = TempConversion(obj_temp)
-
-    # Variable Juggling for Temp Sensor 2
-    die_temp2 = sensor_2.readDieTempC()
-    die_2 = TempConversion(die_temp2)
-    obj_temp2 = sensor_2.readObjTempC()
-    obj_2= TempConversion(obj_temp2)
-
-    # Distance variable setup
-    distance = vl53.range
-
-    # Parse tuple for various axis
-    xAxis = (round(accelerometer.acceleration[0],1))
-    yAxis = (round(accelerometer.acceleration[1],1))
-    zAxis = (round(accelerometer.acceleration[2],1))
-
-    # Print statements for the main sensors 
-    print ('Object temperature: {0:0.3F}*C / {1:0.3F}*F'.format(obj_temp, TempConversion(obj_temp)))
-    print ('Object temperature: {0:0.3F}*C / {1:0.3F}*F'.format(obj_temp2, TempConversion(obj_temp2)))
-    print ("Range: {0}mm".format(vl53.range))
     
-    ser.write (b'Sensor 1 Temperature: %d \n'%round(obj_temp))
-    ser.write (b'Sensor 2 Temperature: %d \n'%round(obj_temp2))
-    ser.write (b'Range: %d '%(vl53.range)+b' mm \n')
-    ser.write (b'X Axis: %d \n'%(xAxis))
-    ser.write (b'Y Axis: %d \n'%(yAxis))
-    ser.write (b'Z Axis: %d \n'%(zAxis))
-
-    # Clean up and format the reading
-    # Create indivudal strngs from tuple data
-    allAxis = ""
-    x = ("x: ","y: ","z: ")
-    zipped = zip(x,accelerometer.acceleration)
-    for item in zipped:
-        a = item[0] + str(round(item[1],1))
-        allAxis += a + "\n"
-    print (allAxis)
-
-    # Write data
-    write_sensors(die_1)
-    time.sleep(1)
+# Multiprocess start loop
+if __name__=='__main__':
+    p1 = Process(target = secondCamera)
+    p1.start()
+    p2 = Process(target = sensors)
+    p2.start()
