720f5c6f0467315a847b3764995faf70c666de86
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-03-19 11:01:24 -0600

    Add files via upload

---
+++ test/pickle_dump.py
@@ -0,0 +1,22 @@
+# Andrew Bruckbauer
+# 3/19/2021
+# Pickle Dump Test
+import pickle
+
+# take user input to take the amount of data
+number_of_data = int(input('Enter the number of data : '))
+data = []
+
+# take input of the data
+for i in range(number_of_data):
+    raw = input('Enter data '+str(i)+' : ')
+    data.append(raw)
+
+# open a file, where you ant to store the data
+file = open('important', 'wb')
+
+# dump information to that file
+pickle.dump(data, file)
+
+# close the file
+file.close()
