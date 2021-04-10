6c5d9efa60cb2c6ba737d7b0252dac48a7240736
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-04-10 08:31:58 -0600

    Add files via upload

--- testing_modes/test_condition.py
+++ testing_modes/test_condition.py
@@ -11,11 +11,33 @@ GPIO.setmode(GPIO.BCM)
 
 wire16=16
 wire19=19
-wire21=20
+wire20=20
+
+'''
+-_-_-_-_-Testing Codes-_-_-_-_-
+Condition 1 (Accel)    16 0 0 
+Condition 2 (Temp)     16 19 0
+Condition 3 (Pi Cam)   19 19 20
+Condition 4 (Motor)    0 0 21
+Condition 5 (Distance) 0 19 21
+Condition 6 (Cam)      16 0 21
+Condition 7 (Launch)   0 0 0
+-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-
+'''
 
 GPIO.setup(wire16,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(wire19,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(wire21,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(wire20,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+print("-_-_-_-_-Testing Codes-_-_-_-_-")
+print("Condition 1 (Accel)    16 0 0 ")
+print("Condition 2 (Temp)     16 19 0")
+print("Condition 3 (Pi Cam)   19 19 20")
+print("Condition 4 (Motor)    0 0 21")
+print("Condition 5 (Distance) 0 19 21")
+print("Condition 6 (Cam)      16 0 21")
+print("Condition 7 (Launch)   0 0 0")
+print("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
 
 def accel():
     #os.system("cd testing_modes")
@@ -34,31 +56,31 @@ def launch():
     os.system('sudo python control_2.py')
 
 while (1):
-    if (GPIO.input(wire16)==0) and GPIO.input(wire19)==1 and (GPIO.input(wire21)==1):
+    if (GPIO.input(wire16)==0) and GPIO.input(wire19)==1 and (GPIO.input(wire20)==1):
         print ("Condition 1")
         accel()
         break
-    elif (GPIO.input(wire16)==0) and GPIO.input(wire19)==0 and (GPIO.input(wire21)==1):
+    elif (GPIO.input(wire16)==0) and GPIO.input(wire19)==0 and (GPIO.input(wire20)==1):
         print ("Condition 2")
         temp()
         
-    elif (GPIO.input(wire16)==0) and GPIO.input(wire19)==0 and (GPIO.input(wire21)==0):
+    elif (GPIO.input(wire16)==0) and GPIO.input(wire19)==0 and (GPIO.input(wire20)==0):
         print ("Condition 3")
         picamera()
         break
-    elif (GPIO.input(wire16)==1) and GPIO.input(wire19)==1 and (GPIO.input(wire21)==0):
+    elif (GPIO.input(wire16)==1) and GPIO.input(wire19)==1 and (GPIO.input(wire20)==0):
         print ("Condition 4")
         motor()
         break
-    elif (GPIO.input(wire16)==1) and GPIO.input(wire19)==0 and (GPIO.input(wire21)==0):
+    elif (GPIO.input(wire16)==1) and GPIO.input(wire19)==0 and (GPIO.input(wire20)==0):
         print ("Condition 5")
         distance()
         break
-    elif (GPIO.input(wire16)==0) and GPIO.input(wire19)==1 and (GPIO.input(wire21)==0):
+    elif (GPIO.input(wire16)==0) and GPIO.input(wire19)==1 and (GPIO.input(wire20)==0):
         print ("Condition 6")
         cam()
         break
-    elif (GPIO.input(wire16)==1) and GPIO.input(wire19)==1 and (GPIO.input(wire21)==1):
+    elif (GPIO.input(wire16)==1) and GPIO.input(wire19)==1 and (GPIO.input(wire20)==1):
         print ("Condition 7")
         print ("LAUNCH CONDITION")
         #launch()
