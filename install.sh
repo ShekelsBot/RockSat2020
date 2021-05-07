8ff271a5aa43552ae49ee6b689fdffaa6e98b1d2
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2021-05-07 11:15:53 -0600

    Overhaul camera control with new usbcamctl module, begin overhaul installer

--- install.sh
+++ install.sh
@@ -1,64 +1,82 @@
 #!/bin/bash
-# Installer 2.0
-cd /home/pi
-mkdir videos
-mkdir data
-mkdir -p /mnt/usb-drive
-#Make it mount directly to /mnt/usb-drive
-apt-get update
-apt-get upgrade -y
-apt-get install python3-pip
-pip3 install --upgrade pip setuptools
-#pip3 install --upgrade setuptools
-apt-get install -y python3 git python3-pip
-#Set Python 3 to default
+# VRSE Payload Software Auto Installation Script
+#   Version 3.0
+
+# Display a nice banner
+echo " "
+echo "  // V.R.S.E (Virtual Reality Space Experience \\\\"
+echo "    Auto Payload Computer Software Setup Script"
+
+# Allow sudo usage without password
+echo "    Allowing sudo usage without the password\n('$USER ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers)"
+sudo echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Update the system
+echo "    Updating all mirrors and packages."
+sudo apt update && sudo apt upgrade -y
+
+# Install required packages
+echo "    Installing the necessary packages (git, vim, python3, python3-pip, i2c-tools, libusb-1.0-0-dev)"
+sudo apt install git vim python3 python3-pip i2c-tools libusb-1.0-0-dev -y
+
+# Install uhubctl
+echo "    Installing uhubctl."
+echo "  git clone https://github.com/mvp/uhubctl"
+git clone https://github.com/mvp/uhubctl
+echo "  cd uhubctl ; sudo make install ; cd ~"
+cd uhubctl ; sudo make install ; cd ~
+
+# Create output directories
+echo "    Create 'data', 'videos', and 'logs' directories."
+mkdir -p data
+mkdir -p videos
+mkdir -p logs
+
+# Setting Python 3 to the default runtime
+echo "    Setting Python v3.x to be the default Python runtime."
 sudo update-alternatives --install /usr/bin/python python $(which python2) 1
 sudo update-alternatives --install /usr/bin/python python $(which python3) 2
-#i2c Tools
-apt-get install python3-dev python3-pip python3-smbus i2c-tools -y
-apt-get install libusb-1.0-0-dev -y
-# update-alternatives --config python
+
+# Install Python packages with pip3
+echo "    Installing Python packages with pip3 (RPI.GPIO, adafruit-blinka)"
 pip3 install RPI.GPIO 
 pip3 install adafruit-blinka
-# Install TMP006
-apt-get install build-essential python-dev python-pip python-smbus git
-pip install RPi.GPIO
-git clone https://github.com/adafruit/Adafruit_Python_TMP.git
-sleep 2
-cd Adafruit_Python_TMP
-python setup.py install
-cd /home/pi
-# Install VL503L0X Library
-pip3 install adafruit-circuitpython-vl53l0x
-# Install ADXL345
-pip3 install adafruit-circuitpython-ADXL34x
-# Install motorkit
-pip3 install adafruit-circuitpython-motorkit
-# Install picamera
-pip3 install picamera
-# Install psutil
-pip3 install psutil
-# Install uhubctl
-git clone https://github.com/mvp/uhubctl
-sleep 2
-cd uhubctl
-make
-make install
-cd ..
-mv RockSat2020_Bruckbauer/camera_scripts /home/pi/camera_scripts
-mv RockSat2020_Bruckbauer/control_2.py /home/pi/control_2.py
-mv RockSat2020_Bruckbauer/save_state.py /home/pi/save_state.py
-mv RockSat2020_Bruckbauer/save_creator.py /home/pi/save_creator.py
-#load inital save_state to 0
-python save_creator.py
-#remove save_creator
-rm -f save_creator.py
-#give executable rights to camera scripts
-cd camera_scripts/
-chmod +x camera_control.sh
-chmod +x camera_control_off.sh
-chmod +x camera_control_on.sh
-echo "Installed"
-sleep 1
-echo "Remember to enable serial, i2c, spi, and pi-camera"
-echo "Run sudo raspi-config to enable these options"
+
+# Old installer stuff VVV
+
+## Install TMP006
+#apt-get install build-essential python-dev python-pip python-smbus git
+#pip install RPi.GPIO
+#git clone https://github.com/adafruit/Adafruit_Python_TMP.git
+#sleep 2
+#cd Adafruit_Python_TMP
+#python setup.py install
+#cd /home/pi
+## Install VL503L0X Library
+#pip3 install adafruit-circuitpython-vl53l0x
+## Install ADXL345
+#pip3 install adafruit-circuitpython-ADXL34x
+## Install motorkit
+#pip3 install adafruit-circuitpython-motorkit
+## Install picamera
+#pip3 install picamera
+## Install psutil
+#pip3 install psutil
+#cd ..
+#mv RockSat2020_Bruckbauer/camera_scripts /home/pi/camera_scripts
+#mv RockSat2020_Bruckbauer/control_2.py /home/pi/control_2.py
+#mv RockSat2020_Bruckbauer/save_state.py /home/pi/save_state.py
+#mv RockSat2020_Bruckbauer/save_creator.py /home/pi/save_creator.py
+##load inital save_state to 0
+#python save_creator.py
+##remove save_creator
+#rm -f save_creator.py
+##give executable rights to camera scripts
+#cd camera_scripts/
+#chmod +x camera_control.sh
+#chmod +x camera_control_off.sh
+#chmod +x camera_control_on.sh
+#echo "Installed"
+#sleep 1
+#echo "Remember to enable serial, i2c, spi, and pi-camera"
+#echo "Run sudo raspi-config to enable these options"
---
+++ usbcamctl.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+"""
+    usbcamctl - This python script makes use of OS calls to uhubctl to control the camera on
+                the end of the arm. (Power On/Off) (Record On/Off)
+
+    Contributors:
+        Konstantin Zaremski
+        Andrew Bruckbauer
+
+    Dependencies:
+        uhubctl (installed binary) <https://github.com/mvp/uhubctl.git>
+"""
+
+# Import dependencies
+import RPi.GPIO as GPIO
+import configparser
+import os
+import time
+
+# Setup GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(pin, GPIO.OUT)
+
+# Load configuration from config.ini
+config = configparser.ConfigParser()
+config.read('./config.ini')
+
+pin = config['usbcamctl']['pin']
+poweron_delay = config['usbcamctl']['poweron_delay']
+setmode_delay = config['usbcamctl']['setmode_delay']
+recordon_delay = config['usbcamctl']['recordon_delay']
+poweroff_delay = config['usbcamctl']['poweroff_delay']
+recordoff_delay = config['usbcamctl']['recordoff_delay']
+
+# Control the camera's powered state from the USB port (uhubctl, then GPIO)
+def power(mode):
+    try:
+        if mode:
+            os.system('uhubctl -a off -l 1-1')
+            time.sleep(3)
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(poweron_delay if mode else poweroff_delay)
+        GPIO.output(pin, GPIO.LOW)
+        return true
+    except:
+        return false
+
+# Control the camera's recording state (GPIO)
+def toggleRecord():
+    try:
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(record_on_seconds)
+        GPIO.output(pin, GPIO.LOW)
+        return true
+    except:
+        return false
