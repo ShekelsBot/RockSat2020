866b186e1cf484e1887eeba212b47705302a70e9
ShekelsBot <49802191+ShekelsBot@users.noreply.github.com>
2021-05-18 12:14:04 -0600

    Add files via upload

--- manualcamctl_test.py
+++ manualcamctl_test.py
@@ -3,10 +3,29 @@
     manualcamctl - This script manually controls the camera and is meant to 
                    be run as a standalone program.
 """
-
 # Import dependencies
 import usbcamctl
 import sys
+
+# Load confifuration from config.ini
+config = configparser.ConfigParser()
+config.read('./config.ini')
+
+#Define and setup
+INHIBIT_1=int(config['pinout']['Inhibit_1'])
+
+# Set GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Setup GPIO
+GPIO.setup(INHIBIT_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#Shorthand
+def inhibit(id):
+    if id == 1: return GPIO.input(INHIBIT_1) == 0
+
+#Define inhibit
+CAM_INHIBIT = inhibit(1)
 
 # Main
 def main(arguments):
@@ -18,7 +37,15 @@ def main(arguments):
     if "usboff" in arguments: usbcamctl.usb(False)
 
 # Entry point
+'''
 if __name__ == "__main__":
+    arguments = sys.argv
+    arguments.pop(0)
+    main(arguments)
+
+'''
+if CAM_INHIBIT:
+    if __name__ == "__main__":
     arguments = sys.argv
     arguments.pop(0)
     main(arguments)
