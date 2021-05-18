51431787c7922c2cd139c7f8894300a20933572d
Konstantin Zaremski <konstantin.zaremski@gmail.com>
2021-05-17 20:48:49 -0600

    Rewrite control.py, usbcamctl.py, persist.py -- asynchronous operations are handled by threading library, not asyncio

---
+++ manualcamctl.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""
+    manualcamctl - This script manually controls the camera and is meant to 
+                   be run as a standalone program.
+"""
+
+# Import dependencies
+import usbcamctl
+import sys
+
+# Main
+async def main(arguments):
+    if "poweron" in arguments: usbcamctl.power(True)
+    if "poweroff" in arguments: usbcamctl.power(False)
+    if "record" in arguments: usbcamctl.toggleRecord()
+    if "mode" in arguments: usbcamctl.toggleMode()
+    if "usbon" in arguments: usbcamctl.usb(True)
+    if "usboff" in arguments: usbcamctl.usb(False)
+
+# Entry point
+if __name__ == "__main__":
+    arguments = sys.argv
+    arguments.pop(0)
+    main(arguments)
