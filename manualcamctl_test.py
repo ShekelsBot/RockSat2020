1f5c46646428e690415910054567f6aacb3e3db7
ShekelsBot <AndrewBruckbauer@gmail.com>
2021-05-19 20:45:58 -0600

    Update manualcamctl_test.py

--- manualcamctl_test.py
+++ manualcamctl_test.py
@@ -3,9 +3,13 @@
     manualcamctl - This script manually controls the camera and is meant to 
                    be run as a standalone program.
 """
+
 # Import dependencies
 import usbcamctl
 import sys
+from time import sleep
+import os
+from RPi import GPIO
 
 # Load confifuration from config.ini
 config = configparser.ConfigParser()
@@ -37,24 +41,30 @@ def main(arguments):
     if "usboff" in arguments: usbcamctl.usb(False)
 
 def camera_testing():
-    usbcamctl.power(True)
-    usbcamctl.usb(False)
-    usbcamctl.toggleRecord()
+    usbcamctl.power(True) #Power on
+    usbcamctl.usb(True) #USB Power off
+    usbcamctl.toggleRecord() #Toggle recording
     sleep(10)
-    usbcamctl.toggleRecord()
-    usbcamctl.usb(True)
+    usbcamctl.toggleRecord() #Stop recording
+    usbcamctl.usb(True) #Turn on power to USB
+
+    # Mount and tranfer files
+    if not os.path.isdir('/mnt/usb'): os.system("sudo mkdir -p /mnt/usb")
+    if not os.path.isdir('video'): os.system("mkdir video")
+    sleep(2)
+    os.system("sudo mount -o ro /dev/sda1 /mnt/usb")
+    sleep(1)
+    os.system("cp /mnt/usb/DCIM/*/*AB.MP4 ./video/")
+    sleep(0.2)
+    os.system("sync")
+    sleep(0.2)
+    os.system("sudo umount /dev/sda1")
+    sleep(1)
+
+    #Power off camera
     usbcamctl.power(False)
 
 # Entry point
-'''
-if __name__ == "__main__":
-    arguments = sys.argv
-    arguments.pop(0)
-    main(arguments)
-
-'''
-
-
 if CAM_INHIBIT:
     print ("Camera ONLY")
     camera_testing()
